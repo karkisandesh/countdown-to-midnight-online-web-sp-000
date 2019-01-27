8848bf2f0037a482a4142b549364aa893e94fbf9
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-27 03:40:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(number)
   until number == 0 
     puts "#{number} SECOND(S)!"
-    countdown_with_sleep(1)
+    countdown_with_sleep(5)
     number -=1 
  end
  puts "HAPPY NEW YEAR!"
